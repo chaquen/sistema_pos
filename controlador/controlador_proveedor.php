8ce3b7080dc8c53321be19332c685d2e519f7bcd
Adrian Guzman <edgar.guzman21@gmail.com>
2016-06-13 12:17:16 -0500

    Archivo para probar funcionamiento

---
+++ controlador/controlador_proveedor.php
@@ -0,0 +1,60 @@
+<?php
+header('Content-Type:text/html; Charset="UTF-8"');    
+include("../datos/modelo.php");
+include("../utilidades/utilidades.php");
+if(isset($_POST['datos'])){
+    $post=  json_decode($_POST['datos']);
+    $operacion=$post->operacion;
+    $objeto= new MyClase();//Mi clase es una clase sin implementar que me sirve de modelo 
+    /*
+        AQUI INSTANCIO EL OBJETO QUE DESEO USAR EN EL CONTROLADOR
+     */
+    
+    $respuestaServidor=new formatoRespuestaServidor();
+    switch($operacion){
+        case "crear":
+            
+            /*
+             * AQUI DOY VALOR A CADA UNA DE LAS PROPIEDADES DE LA CLASE PARA INSERTAR LOS VALORES
+             */
+            /*
+             * Para acceder a cada una de las propiedaes enviadas en el metodo POST se debe acceder desde objeto 
+             * $post a la proiedad datos ejemplo
+             * $post->datos->miDatoEnviadoDesdeElCliente
+             */
+            echo json_encode($objeto->crear_registro());
+            
+            break;
+        case "actualizar":
+            /*
+             * AQUI DOY VALOR A CADA UNA DE LAS PROPIEDADES DE LA CLASE PARA ACTUALIZAR LOS VALORES
+             */
+            /*
+             * Para acceder a cada una de las propiedaes enviadas en el metodo POST se debe acceder desde objeto 
+             * $post a la proiedad datos ejemplo
+             * $post->datos->miDatoEnviadoDesdeElCliente
+             */
+            echo json_encode($objeto->actualizar_recurso());
+            break;
+        case "eliminar":
+            
+            /*
+             * AQUI DOY VALOR DEL ISD QUE DESEO ELIMINAR
+             */
+            /*
+             * Para acceder a cada una de las propiedaes enviadas en el metodo POST se debe acceder desde objeto 
+             * $post a la proiedad datos ejemplo
+             * $post->datos->miDatoEnviadoDesdeElCliente
+             */
+            echo json_encode($objeto->eliminar_registro());
+            break;
+        case "consultar":
+            echo json_encode($objeto->obtener_registro_todos_los_registros());
+            break;
+        default :
+            echo json_encode(array("respuesta"=>FALSE,"mensaje"=>"Por favor defina una operacion o agrege una opcion en el swicth"));
+            break;
+    }
+}else{
+    echo json_encode(array("respuesta"=>FALSE,"mensaje"=>"Por favor ingrese datos en la peticion"));
+}
